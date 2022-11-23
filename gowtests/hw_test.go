7f58bfcac9bc8a63afc2c303f291a962dfcd9f1e
keatmin <keatmin.woo@shippit.com>
2022-11-24 01:42:52 +1100

    [keat] add const and deal with empty string

--- gowtests/hw_test.go
+++ gowtests/hw_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("mate")
-	want := "Hello mate"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("mate")
+		want := "Hello mate"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say world when empty string is provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // to notify test suite that this method is a helper. Failure will be reported in function call rather than test helper
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
