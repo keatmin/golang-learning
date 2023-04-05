b3e1074b88c857348c9ba377b34d122e9477d60d
keatmin <keatmin3@gmail.com>
2023-04-05 20:52:32 +1000

    [ktmn] refactor(test): pointers after errcheck

--- gowtests/6_pointers/pointers_test.go
+++ gowtests/6_pointers/pointers_test.go
@@ -13,7 +13,8 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -39,6 +40,15 @@ func assertError(t testing.TB, got, want error) {
 
 }
 
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+
+	}
+
+}
 func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 	t.Helper()
 	got := wallet.Balance()
