61b1679d088f73ac5a41778e10de56f8c0501316
keatmin <keatmin3@gmail.com>
2023-04-05 20:37:29 +1000

    [ktmn] refactor(tests): pointers

--- gowtests/6_pointers/pointers.go
+++ gowtests/6_pointers/pointers.go
@@ -17,7 +17,7 @@ func (w *Wallet) Deposit(amount Bitcoin) { // In golang method arguments are cop
 
 func (w *Wallet) Withdraw(amount Bitcoin) error { // In golang method arguments are copied hence pointers are used to update by pointing at the value
 	if amount > w.balance {
-		return errors.New("oh no")
+		return errors.New("cannot withdraw, insufficient fund")
 	}
 
 	w.balance -= amount
--- gowtests/6_pointers/pointers_test.go
+++ gowtests/6_pointers/pointers_test.go
@@ -5,6 +5,18 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one") // will stop the test if called
+		}
+		if got.Error() != want {
+			t.Errorf("got %q want %q", got, want)
+
+		}
+
+	}
+
 	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
 		t.Helper()
 		got := wallet.Balance()
@@ -31,10 +43,7 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
+		assertError(t, err, "cannot withdraw, insufficient fund")
 		assertBalance(t, wallet, startingBalance)
-
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
 	})
 }
