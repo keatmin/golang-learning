9e7e3b403471195046d19fc8792507418faa63ec
keatmin <keatmin3@gmail.com>
2023-05-14 00:55:23 +1000

    [ktmn] style(order): rearrange code

--- gowtests/7_maps/maps.go
+++ gowtests/7_maps/maps.go
@@ -1,7 +1,5 @@
 package maps
 
-type Dictionary map[string]string
-
 const (
 	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
 	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
@@ -13,6 +11,8 @@ type DictionaryErr string
 func (e DictionaryErr) Error() string {
 	return string(e)
 }
+
+type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
