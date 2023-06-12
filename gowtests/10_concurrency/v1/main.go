a4e0edcb8bdcff0c7fbf382f428dfbd261f7752e
keatmin <keatmin3@gmail.com>
2023-06-12 22:44:59 +1000

    [ktmn]  update v1

--- gowtests/10_concurrency/v1/main.go
+++ gowtests/10_concurrency/v1/main.go
@@ -7,6 +7,7 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 
 	for _, url := range urls {
 		go func() {
+			// this will cause issue as the function has different url in the loop and the value is not copied
 			results[url] = wc(url)
 		}()
 	}
