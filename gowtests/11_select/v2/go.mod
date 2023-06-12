cf8735e266c98e2c54d690716dfc988f79c77fa7
keatmin <keatmin3@gmail.com>
2023-06-12 23:31:38 +1000

    [ktmn] add v2 select

---
+++ gowtests/11_select/v2/go.mod
@@ -0,0 +1,3 @@
+module golang-playground/select
+
+go 1.20
---
+++ gowtests/11_select/v2/select_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	// HandlerFunc needs a function that takes a ResponseWriter and a Request
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(10 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
